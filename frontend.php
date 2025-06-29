31a8d6df435464ba5065bc5e9a72b3514db47c2a
Tim Otterbach <tim@otterba.ch>
2025-06-29 22:29:15 +0200

    test with copilot

---
+++ frontend.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <div id="scorebox_t1" class="scorebox">
+            <div class="scorebox_headline">Team 1</div>
+            <div class="scorebox_scoreline">Scoreline 1</div>
+        </div>
+        <div id="scorebox_t2" class="scorebox">
+            <div class="scorebox_headline">Team 2</div>
+            <div class="scorebox_scoreline">Scoreline 2</div>
+        </div>
+        <div id="scorebox_t3" class="scorebox">
+            <div class="scorebox_headline">Team 3</div>
+            <div class="scorebox_scoreline">Scoreline 3</div>
+        </div>
+        <div id="scorebox_t4" class="scorebox">
+            <div class="scorebox_headline">Team 4</div>
+            <div class="scorebox_scoreline">Scoreline 4</div>
+        </div>
+        <div id="scorebox_t5" class="scorebox">
+            <div class="scorebox_headline">Team 5</div>
+            <div class="scorebox_scoreline">Scoreline 5</div>
+        </div>
+        <div id="scorebox_t6" class="scorebox">
+            <div class="scorebox_headline">Team 6</div>
+            <div class="scorebox_scoreline">Scoreline 6</div>
+        </div>
+        <div id="scorebox_t7" class="scorebox">
+            <div class="scorebox_headline">Team 7</div>
+            <div class="scorebox_scoreline">Scoreline 7</div>
+        </div>
+        <div id="scorebox_t8" class="scorebox">
+            <div class="scorebox_headline">Team 8</div>
+            <div class="scorebox_scoreline">Scoreline 8</div>
+        </div>
+    </body>
+</html>
