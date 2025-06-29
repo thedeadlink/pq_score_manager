075ebeb087b362beda29d17b5814f7d77c22e5df
Tim Otterbach <tim@otterba.ch>
2025-06-29 23:15:08 +0200

    style rework coreboard for gameplay

--- frontend.php
+++ frontend.php
@@ -2,39 +2,38 @@
     <head>
         <link rel="stylesheet" type="text/css" href="styles.css">
         <link href="https://fonts.googleapis.com/css2?family=ADLaM+Display&display=swap" rel="stylesheet">
+        <meta http-equiv="refresh" content="5">
     </head>
     <body>
-        <div id="scorebox_t1" class="scorebox">
-            <div class="scorebox_headline">Team 1</div>
-            <div class="scorebox_scoreline">Scoreline 1</div>
-        </div>
-        <div id="scorebox_t2" class="scorebox">
-            <div class="scorebox_headline">Team 2</div>
-            <div class="scorebox_scoreline">Scoreline 2</div>
-        </div>
-        <div id="scorebox_t3" class="scorebox">
-            <div class="scorebox_headline">Team 3</div>
-            <div class="scorebox_scoreline">Scoreline 3</div>
-        </div>
-        <div id="scorebox_t4" class="scorebox">
-            <div class="scorebox_headline">Team 4</div>
-            <div class="scorebox_scoreline">Scoreline 4</div>
-        </div>
-        <div id="scorebox_t5" class="scorebox">
-            <div class="scorebox_headline">Team 5</div>
-            <div class="scorebox_scoreline">Scoreline 5</div>
-        </div>
-        <div id="scorebox_t6" class="scorebox">
-            <div class="scorebox_headline">Team 6</div>
-            <div class="scorebox_scoreline">Scoreline 6</div>
-        </div>
-        <div id="scorebox_t7" class="scorebox">
-            <div class="scorebox_headline">Team 7</div>
-            <div class="scorebox_scoreline">Scoreline 7</div>
-        </div>
-        <div id="scorebox_t8" class="scorebox">
-            <div class="scorebox_headline">Team 8</div>
-            <div class="scorebox_scoreline">Scoreline 8</div>
-        </div>
+        <table class="scorebox_table">
+            <tr>
+                <td class="scorebox_headline_normal">Callies and Friends</td>
+                <td class="scorebox_headline_normal">Hopfen und Verstand</td>
+                <td class="scorebox_headline_normal">6 Helle</td>
+                <td class="scorebox_headline_small">Luxusbabypuppenwagen Oberfischbach e.v.</td>
+                <td class="scorebox_headline_normal">Team 5</td>
+                <td class="scorebox_headline_normal">Team 6</td>
+                <td class="scorebox_headline_normal">Team 7</td>
+                <td class="scorebox_headline_normal">Team 8</td>
+                <td class="scorebox_headline_normal">Team x</td>
+                <td class="scorebox_headline_normal">Team 2</td>
+                <td class="scorebox_headline_normal">Team 3</td>
+                <td class="scorebox_headline_normal">Team 4</td>
+            </tr>
+            <tr>
+                <td class="scorebox_scoreline">23</td>
+                <td class="scorebox_scoreline">12</td>
+                <td class="scorebox_scoreline">3</td>
+                <td class="scorebox_scoreline">33</td>
+                <td class="scorebox_scoreline">564</td>
+                <td class="scorebox_scoreline">4</td>
+                <td class="scorebox_scoreline">42</td>
+                <td class="scorebox_scoreline">22</td>
+                <td class="scorebox_scoreline">23</td>
+                <td class="scorebox_scoreline">12</td>
+                <td class="scorebox_scoreline">3</td>
+                <td class="scorebox_scoreline">33</td>
+            </tr>
+        </table>
     </body>
 </html>
