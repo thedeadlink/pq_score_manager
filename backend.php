68f1cda9202a521f769733e75ab305583f469ce8
Tim Otterbach <tim@otterba.ch>
2026-02-01 01:58:46 +0100

    no cache added

--- backend.php
+++ backend.php
@@ -51,6 +51,9 @@ $htmlHead = '
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <script>
         function toggleRename(teamId) {
             const renameControls = document.getElementById("rename-controls-" + teamId);
